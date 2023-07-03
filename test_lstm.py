1a1510ff46a4e2bd67421d83fd60393aef753dd1
Edson <110080859+epdavid1@users.noreply.github.com>
2023-07-03 17:04:20 +0800

    Add files via upload

---
+++ test_lstm.py
@@ -0,0 +1,16 @@
+from tsai.all import *
+import pandas as pd
+import numpy as np
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+tst_noise = load_learner('lstm_noise')
+tst_test = pd.read_csv('test.csv')
+
+iter = np.zeros(400)
+for i in range(0,400):
+	start = time.time()
+	tst_noise.get_X_preds(tst_test.iloc[0].to_numpy().reshape(1,1,10001))	[2].astype(int)
+	iter[i] = time.time()-start
+print('Average detection time for 400 trials = '+str(int(np.round(np.mean(iter)*1000)))+' ms')
